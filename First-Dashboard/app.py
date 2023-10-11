bc68e9773eeb8a153c6ecff3bbb8cd11c0fd87d8
AugustoAleman <a01660702@tec.mx>
2023-10-11 12:03:00 -0600

    Updated CSS Styles of dashboard + Updated gitignore

--- First-Dashboard/app.py
+++ First-Dashboard/app.py
@@ -20,28 +20,36 @@ app.layout = html.Div([
     html.Link(href="/assets/styles.css", rel="stylesheet"),
     # Navbar with logo and options
     html.Div([
-        html.Img(src='your_logo.png', className='logo'),  # Customize logo path
+        html.Img(src='assets/src/liverpool-logo.png', className='logo'),  # Customize logo path
         html.Div([
-            html.A('Option 1', className='nav-option'),
-            html.A('Option 2', className='nav-option'),
-        ], className='nav-options'),
+            html.Div(html.A('INICIO'), style = {'text-decoration': 'underline'}, className='nav-option'),
+            html.Div(html.A('USUARIOS'), className='nav-option'),
+            html.Div(html.A('PANEL DE CONTROL'), className='nav-option'),
+            html.Div(html.Button('SALIR', className='rounded-button')),
+        ], className='nav-options')
+
     ], className='navbar'),
     
     # Two-column layout
     html.Div([
         # Left column
         html.Div([
-            html.H1('Dashboard Title', className='title'),
-            html.H3('Subtitle', className='subtitle'),
-            html.P('Description paragraph goes here.', className='description'),
+            html.H1('Liverpool Human Analytics', className='title'),
+            html.H3('Bienvenido al Dashboard de Human Analytics de Liverpool', className='subtitle'),
+            html.P('Explora los datos más recientes de Human Analytics dentro de Liverpool. Selecciona si deseas visualizar un sector o periodo específicos.', className='description'),
+            html.H4('Selecciona un sector', className='picker-description'),
             dcc.Dropdown(
                 id='dropdown',
                 options=[
-                    {'label': 'Option 1', 'value': 'option1'},
-                    {'label': 'Option 2', 'value': 'option2'},
+                    {'label': 'Todos', 'value': 'Todos'},
+                    {'label': 'Liverpool', 'value': 'Liverpool'},
+                    {'label': 'Suburbia', 'value': 'Suburbia'},
+                    {'label': 'CeDis', 'value': 'CeDis'},
+                    {'label': 'Otros', 'value': 'Otros'},
                 ],
-                value='option1',
+                value='Todos',
             ),
+            html.H4('Selecciona un periodo', className='picker-description'),
             dcc.DatePickerRange(
                 id='date-picker',
                 start_date=datetime(2023, 1, 1),
@@ -51,17 +59,23 @@ app.layout = html.Div([
         
         # Right column with charts
         html.Div([
-            html.Div('Chart 1 Title', className='chart-title'),
-            html.Hr(),  # Horizontal line
-            dcc.Graph(
-                figure=px.bar(data, x='Category', y='Value', title='Bar Chart'),
-            ),
-            html.Div('Chart 2 Title', className='chart-title'),
-            html.Hr(),
-            dcc.Graph(
-                figure=px.pie(data, names='Category', values='Value', title='Pie Chart'),
-            ),
-            # Add more charts here as needed
+            html.Div([
+                html.Div([
+                    html.Div('Chart 1 Title', className='chart-title'),
+                    html.Hr(className='divider'),  # Horizontal line
+                    dcc.Graph(
+                        figure=px.bar(data, x='Category', y='Value', title='Bar Chart'),
+                    ),
+                ], className = 'chart-container'),
+                html.Div([
+                    html.Div('Chart 2 Title', className='chart-title'),
+                    html.Hr(className='divider'),
+                    dcc.Graph(
+                        figure=px.pie(data, names='Category', values='Value', title='Pie Chart'),
+                    ),
+                ], className = 'chart-container'),
+                # Add more charts here as needed
+            ], className='charts-column')
         ], className='right-column', style={'width': '70%'}),
     ], className='two-column-layout'),
 ])
